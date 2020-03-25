6639a2fa28dc687885e04132e4b201ae618969d2
Владислав Данилов <danilov@RIT-L005.rit.va>
2020-03-25 10:20:11 +0300

    add tests for accordion, code refactoring

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, ReactElement } from 'react';
+import { DropDownButton } from 'components/Button';
+
 import * as S from './Accordion.style';
 
 type AccordionProps = {
-  render(toogle: Function, isExpanded: boolean): void;
+  render?: (toogle: Function, isExpanded: boolean) => ReactElement;
+  title?: string;
   isExpanded: boolean;
 };
 
@@ -10,16 +13,21 @@ export const Accordion: React.FC<AccordionProps> = ({
   children,
   render,
   isExpanded,
+  title,
 }) => {
   const [expanded, setExpanded] = useState(isExpanded);
   const toggleExpanded = (): void => setExpanded(!expanded);
 
   return (
     <>
-      {render(toggleExpanded, expanded)}
-      <S.Accordion expanded={expanded}>
-        <S.Content expanded={expanded}>{children}</S.Content>
-      </S.Accordion>
+      {render ? (
+        render(toggleExpanded, expanded)
+      ) : (
+        <DropDownButton onClick={toggleExpanded} expanded={expanded}>
+          {title}
+        </DropDownButton>
+      )}
+      <S.Accordion expanded={expanded}>{children}</S.Accordion>
     </>
   );
 };
