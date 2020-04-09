63be8f166145306e7b43410545ed64d698acbfff
Илья Волосюк <volosyuk@RIT-L003.rit.va>
2020-04-09 08:35:39 +0300

    add check for profile on sign-in page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -4,6 +4,7 @@ import {
   RouteComponentProps,
   Router,
   useNavigate,
+  useMatch,
 } from '@reach/router';
 import { useSelector, useDispatch } from 'react-redux';
 import { requestProfile } from 'redux/profile/thunks';
@@ -12,6 +13,7 @@ import {
   getProfileInfo,
 } from 'redux/profile/selectors';
 
+import { SignIn } from 'pages/signIn/signIn';
 import { Soldiers } from 'pages/soldiers/soldiers';
 import { PersonnelDetails } from 'pages/personnelDetails/personnelDetails';
 
@@ -19,11 +21,16 @@ import { Header } from './components/Header';
 import { LoadingScreen } from './components/LoadingScreen';
 import { NotFound } from './components/NotFound';
 
+const SIGN_IN_URL = '/sign-in';
+const PERSONNEL_URL = '/personnel';
+const PERSONNEL_DETAILS_URL = '/personnel-details/:id';
+
 const Main: React.FC<RouteComponentProps> = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const profileInfo = useSelector(getProfileInfo);
   const isAuthChecked = useSelector(isAuthCheckedSelector);
+  const isSignInPage = useMatch(SIGN_IN_URL);
 
   useEffect(() => {
     if (!isAuthChecked) {
@@ -35,19 +42,25 @@ const Main: React.FC<RouteComponentProps> = () => {
     return <LoadingScreen />;
   }
 
-  if (isAuthChecked && !profileInfo) {
-    navigate('/sign-in');
+  if (!isSignInPage && isAuthChecked && !profileInfo) {
+    navigate(SIGN_IN_URL);
+    return null;
+  }
+
+  if (isSignInPage && isAuthChecked && profileInfo) {
+    navigate(PERSONNEL_URL);
     return null;
   }
 
   return (
     <>
-      <Header />
+      {!isSignInPage && <Header />}
       <Router>
         <NotFound default />
-        <Redirect from="/" noThrow to="/personnel" />
-        <Soldiers path="/personnel" />
-        <PersonnelDetails path="/personnel-details/:id" />
+        <Redirect from="/" noThrow to={PERSONNEL_URL} />
+        <SignIn path={SIGN_IN_URL} />
+        <Soldiers path={PERSONNEL_URL} />
+        <PersonnelDetails path={PERSONNEL_DETAILS_URL} />
       </Router>
     </>
   );
