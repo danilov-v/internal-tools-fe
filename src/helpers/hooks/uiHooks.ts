56ec7622f5e6226ca3ff011887022d649488a671
Владислав Данилов <danilov@RIT-L005.rit.va>
2020-04-15 10:55:48 +0300

    prevent scrolling when dialog open

--- src/helpers/hooks/uiHooks.ts
+++ src/helpers/hooks/uiHooks.ts
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useDialog = (): [boolean, () => void] => {
   const [isOpen, toggleDialog] = useState(false);
   const toggle = (): void => toggleDialog(!isOpen);
+
+  useEffect((): void => {
+    document.body.style.overflow = isOpen ? 'hidden' : 'unset';
+  }, [isOpen]);
+
   return [isOpen, toggle];
 };
